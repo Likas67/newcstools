e97c7832ce0f2d4b26c35001076b9ae4e6dd74e3
Likas67 <likaseina@gmail.com>
2026-03-01 16:22:52 +0200

    pakeistas main ir vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist', // Netlify publish directory
+  },
 });
